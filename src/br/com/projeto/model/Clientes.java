4620c4c2afd9b189cd7ec19407a10d8a8a474a84
mlima3696 <mjuniorlima3@gmail.com>
2020-08-03 22:12:25 -0300

    Criando o método Cadastrar Cliente na classe ClienteDAO

--- src/br/com/projeto/model/Clientes.java
+++ src/br/com/projeto/model/Clientes.java
@@ -25,7 +25,7 @@ public class Clientes {
     private String complemento;
     private String bairro;
     private String cidade;
-    private String uf;
+    private String estado;
     
     //Getters e Setters
 
@@ -133,13 +133,15 @@ public class Clientes {
         this.cidade = cidade;
     }
 
-    public String getUf() {
-        return uf;
+    public String getEstado() {
+        return estado;
     }
 
-    public void setUf(String uf) {
-        this.uf = uf;
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
+
+   
 
     
 }
---
+++ src/br/com/projetos/dao/ClientesDAO.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projetos.dao;
+
+import br.com.projeto.jdbc.ConnectionFactory;
+import br.com.projeto.model.Clientes;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ACER
+ */
+public class ClientesDAO {
+    
+    private Connection con;
+    public ClientesDAO(){
+    this.con= new ConnectionFactory().getConnection();
+    }
+    
+    //Metodo cadastrarCliente
+    public void cadastarCliente(Clientes obj){
+        try {
+            //1 passo - criar o comando sql
+            String sql = "insert into tb_clientes  (nome,rg,cpf,email,telefone,celular,cep,endereco,numero,complento,bairro,cidade,estado) "
+                    + "valeus (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            //2 passo - conectar no banco de dados e organizar o comando sql
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, obj.getNome());
+            pst.setString(2, obj.getRg());
+            pst.setString(3, obj.getCpf());
+            pst.setString(4, obj.getEmail());
+            pst.setString(5, obj.getTelefone());
+            pst.setString(6, obj.getCelular());
+            pst.setString(7, obj.getCep());
+            pst.setString(8, obj.getEndereco());
+            pst.setInt(9, obj.getNumero());
+            pst.setString(10, obj.getComplemento());
+            pst.setString(11, obj.getBairro());
+            pst.setString(12, obj.getCidade());
+            pst.setString(13, obj.getEstado());
+            
+            //3 passo - executar o comkando sql
+            pst.execute();
+            pst.close();
+           
+            JOptionPane.showMessageDialog(null, "Cadastrado com sucesso");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,"Erro: " + e);
+            System.out.println(e);
+        }
+    }
+     //Metodo Alterar
+    public void alterarCliente(){
+   
+    }
+     //MetodoExcluir
+    public void excluirCliente(){
+   
+    }
+   
+    
+}
