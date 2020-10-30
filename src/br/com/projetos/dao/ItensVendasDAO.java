85974d2a002914e8c959ab1f9836664ce888cd56
mlima3696 <mjuniorlima3@gmail.com>
2020-10-30 19:06:37 -0300

    Criando o método ListarItens de uma Venda na classe ItemVendaDAO

--- src/br/com/projetos/dao/ItensVendasDAO.java
+++ src/br/com/projetos/dao/ItensVendasDAO.java
@@ -6,9 +6,16 @@
 package br.com.projetos.dao;
 
 import br.com.projeto.jdbc.ConnectionFactory;
+import br.com.projeto.model.Cliente;
 import br.com.projeto.model.ItemVendas;
+import br.com.projeto.model.Produtos;
+import br.com.projeto.model.Vendas;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -45,4 +52,46 @@ public class ItensVendasDAO {
             JOptionPane.showMessageDialog(null, "Erro: " + e);
         }
     }
+    
+    //Metodo que lista Itens de uma venda por Id
+   public List<ItemVendas>listarItensPorVenda(int venda_id){
+            try {
+                // 1 passo criar a lista
+                List<ItemVendas> lista = new ArrayList<>();
+                
+                // 2 passo - criar o sql, organizar e executar
+                
+                String sql = "select i.id,p.descricao, i.qtd, p.preco, i.subtotal from tb_itensvendas as i " +
+                             "inner join tb_produtos as p on(i.produto_id = p.id) where i.venda_id = ?";
+                //3 Passo - conectar o banco de dados e organizar o comando sql
+                PreparedStatement pst = con.prepareStatement(sql);
+                
+                pst.setInt(1, venda_id);
+                
+                ResultSet rs = pst.executeQuery();
+                
+                while(rs.next()){
+                ItemVendas item = new ItemVendas();
+                Produtos prod =new Produtos();
+                
+                item.setId(rs.getInt("i.id"));
+                prod.setDescricao(rs.getString("p.descricao"));
+                item.setQtd(rs.getInt("i.qtd"));
+                prod.setPreco(rs.getDouble("p.preco"));
+                item.setSubtotal(rs.getInt("i.subtotal"));
+                
+                item.setProduto(prod);
+                
+                
+                lista.add(item);
+                }
+                
+                return lista;
+                
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Erro " + e);
+                return null;
+            }
+        }
+    
 }
